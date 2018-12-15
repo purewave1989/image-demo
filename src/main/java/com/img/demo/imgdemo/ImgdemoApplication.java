e6df396f99845b1563dcd7dae70ae37de4d071a0
xiaqingbo <xia_qingbo@163.com>
2018-12-15 11:09:41 +0800

    demo init

---
+++ src/main/java/com/img/demo/imgdemo/ImgdemoApplication.java
@@ -0,0 +1,12 @@
+package com.img.demo.imgdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgdemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/img/demo/imgdemo/ImgdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.img.demo.imgdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImgdemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
